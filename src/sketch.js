36d81a2d93cf4d441c7fa6d409ebba708499748a
DESKTOP-QQB1ARU\Carl Lee <287143005@qq.com>
2018-05-22 23:57:17 +1000

    update game

--- src/sketch.js
+++ src/sketch.js
@@ -1,43 +1,48 @@
-var cols = 20;
-var rows = 20;
-var unit = 40;
-var debug;
-var human = [];
+var balls = [];
 
 function setup() {
   createCanvas(800, 800);
   background(200);
 
-  //init human...
-  for(let i = 0; i<20; i++){
-    human.push(new Character(40*floor(random(0,20)), 40*floor(random(0,20))));
-    // human.draw();
+  for(let i = 0; i<1000; i++){
+    let ball = {
+      x:random(0,800),
+      y:random(0,800),
+      color:0
+    };
+    balls.push(ball);
   }
-  
-
 }
 
+function isCollision(a, b){
+  let dist = (a.x - b.x)**2 + (a.y - b.y)**2;
+  if(dist < 400){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 function draw() {
   background(200);
-  for(let i = 0; i < cols; i++){
-    for(let j = 0; j<rows; j++){
-      fill(200);
-      rect(i*unit,j*unit,unit,unit);
-      // fill(cols,rows,255);
+  for(let i = 0; i<1000; i++){
+    fill(255,200,balls[i].color);
+    balls[i].x += random(-1,1);
+    balls[i].y += random(-1,1);
+    ellipse(balls[i].x, balls[i].y, 10, 10);
+  }
+
+  for(let i = 0; i<1000; i++){
+    for(let j = 0; j<1000; j++){
+      if(i!=j && isCollision(balls[i], balls[j])){
+        balls[i].color = 200;
+        balls[j].color = 200;
+      }else{
+        balls[i].color = 0;
+        balls[j].color = 0;
+      }
     }
   }
-  for(let i=0; i<human.length;i++){
-    human[i].draw();
-  }
   
-  // noLoop();
-
 }
 
-function mousePressed(){
-  for(let i =0; i<human.length; i++){
-    // console.log(human[i])
-    human[i].clicked();
-  }
-}
